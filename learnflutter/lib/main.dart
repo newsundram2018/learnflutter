15b668a486b1538b27f210c011a7ea9bbbf42304
Sundram <newsundram2018@gmail.com>
2020-12-30 23:25:36 +0530

    Container practice code

--- learnflutter/lib/main.dart
+++ learnflutter/lib/main.dart
@@ -17,10 +17,30 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text("Awesome App"),
+        title: Text("Awesome App"),
       ),
-      body: Container(
-        child: Text("Hello Everyone"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(colors: [
+                Colors.yellow,
+                Colors.pink,
+              ]),
+              boxShadow: [
+                BoxShadow(color: Colors.grey, blurRadius: 10),
+              ]),
+          child: Text(
+            "I am a box",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
     );
   }
